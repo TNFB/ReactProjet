5705a779bc220ebc3b5d6195557f26d534d8d8cc
TNFB <tom.herault4986@gmail.com>
2024-04-19 17:15:50 +0200

    Base de cours

---
+++ api/models/freelances.js
@@ -0,0 +1,85 @@
+const freelances = [
+	{
+		id: '1',
+		name: 't-shirt',
+		taille: 'XS',
+		cover: 'https://apikarminecorp.tomherault.fr/images/1.jpg',
+		price: 15,
+		category: 'haut',
+		isSpecialOffer: true,
+		confort: 3
+	},
+	{
+		id: '2',
+		name: 'pull',
+		taille: 'S',
+		cover: 'https://apikarminecorp.tomherault.fr/images/2.jpg',
+		price: 10,
+		category: 'haut',
+		isSpecialOffer: false,
+		confort: 2
+	},
+	{
+		id: '3',
+		name: 'pantalon',
+		taille: 'M',
+		cover: 'https://apikarminecorp.tomherault.fr/images/3.jpg',
+		price: 33,
+		category: 'bas',
+		isSpecialOffer: true,
+		confort: 4
+	},
+	{
+		id: '4',
+		name: 'short',
+		taille: 'L',
+		cover: 'https://apikarminecorp.tomherault.fr/images/4.jpg',
+		price: 23,
+		category: 'bas',
+		isSpecialOffer: false,
+		confort: 5
+	},
+	{
+		id: '5',
+		name: 'jupe',
+		taille: 'XL',
+		cover: 'https://apikarminecorp.tomherault.fr/images/5.jpg',
+		price: 12,
+		category: 'bas',
+		isSpecialOffer: false,
+		confort: 2
+	},
+	{
+		id: '6',
+		name: 'chemisier',
+		taille: 'S',
+		cover: 'https://apikarminecorp.tomherault.fr/images/6.jpg',
+		price: 16,
+		category: 'haut',
+		isSpecialOffer: true,
+		confort: 3
+	},
+	{
+		id: '7',
+		name: 'écharpe',
+		taille: 'M',
+		cover: 'https://apikarminecorp.tomherault.fr/images/7.jpg',
+		price: 20,
+		category: 'haut',
+		isSpecialOffer: true,
+		confort: 4
+	},
+	{
+		id: '8',
+		name: 'robe',
+		taille: 'L',
+		cover: 'https://apikarminecorp.tomherault.fr/images/8.jpg',
+		price: 30,
+		category: 'ensemble',
+		isSpecialOffer: false,
+		confort: 1
+	},
+	
+]
+
+module.exports = freelances
---
+++ backend/app.js
@@ -0,0 +1,195 @@
+const express = require('express'); //on importe express
+const cors = require('cors'); // Importez le module cors
+const mysql = require('mysql2'); //on importe mysql2 
+const multer = require('multer'); // Importez le module multer
+const bcrypt = require('bcrypt'); // Importez le module bcrypt
+const jwt = require('jsonwebtoken'); // Importez le module jsonwebtoken
+
+const bodyParser = require('body-parser');
+
+//app sera notre application express
+const app = express();
+
+
+
+// Activez CORS pour toutes les routes
+app.use(cors());// Connexion à MySQL
+app.use(bodyParser.json());
+
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'frontback',
+    password: 'frontbackESEO2024',
+    database: 'front_back',
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Erreur de connexion à MySQL :', err);
+    } else {
+        console.log('Connexion à MySQL réussie !');
+    }
+});
+// Exemple de requête SELECT à partir de votre table vêtement
+app.get('/', async (req, res, next) => {
+    connection.query('SELECT * FROM vetement', (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la requête SELECT :', error);
+            res.status(500).json({ error: 'Erreur serveur lors de la requête SELECT.' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+//je récupère un seul vêtement en fonction de son id
+app.get('/:id', async (req, res, next) => {
+    const vetementId = req.params.id;
+    connection.query('SELECT * FROM vetement WHERE id = ?', [vetementId], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la requête SELECT :', error);
+            res.status(500).json({ error: 'Erreur serveur lors de la requête SELECT.' });
+        } else {
+            if (results.length > 0) {
+                // Si des résultats sont trouvés, renvoyer le premier élément (le vêtement trouvé)
+                res.status(200).json(results[0]);
+            } else {
+                // Si aucun résultat n'est trouvé, renvoyer une réponse 404
+                res.status(404).json({ error: 'Aucun vêtement trouvé avec cet ID.' });
+            }
+        }
+    });
+});
+//on modifie un seul vêtement en fonction de son id   
+app.put('/:id', async (req, res, next) => {
+    const vetementId = req.params.id;
+    const updatedVetementData = req.body;
+    connection.query('UPDATE vetement SET ? WHERE id = ?', [updatedVetementData, vetementId], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la requête UPDATE :', error);
+            res.status(500).json({ error: 'Erreur serveur lors de la requête UPDATE.' });
+        } else {
+            // Après la mise à jour, récupérez à nouveau les données mises à jour
+            const selectQuery = 'SELECT * FROM vetement WHERE id = ?';
+            connection.query(selectQuery, [vetementId], (selectError, selectResults) => {
+                if (selectError) {
+                    console.error('Erreur lors récupération données mises à jour :', selectError);
+                    res.status(500).json({ error: 'Erreur serveur récupération données mises à jour.' });
+                } else {
+                    if (selectResults.length > 0) {
+                        res.status(200).json(selectResults[0]); // Renvoyez données mises à jour en réponse
+                    } else {
+                        res.status(404).json({ error: 'L\'objet n\'existe pas.' });
+                    }
+                }
+            });
+        }
+    });
+});
+
+// Configuration du dossier où les images seront stockées
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const uploadDir = './images';
+        callback(null, uploadDir);
+    },
+    filename: (req, file, callback) => {
+        const fileName = `${Date.now()}-${file.originalname}`;
+        callback(null, fileName);
+    }
+});
+const upload = multer({ storage: storage });
+// Route POST pour télécharger une image
+// Définir le dossier contenant vos images comme dossier statique
+app.use('/images', express.static('images'));
+
+// Route pour gérer le téléchargement d'images
+app.post('/upload', upload.single('image'), async (req, res) => {
+    try {
+        // req.file contient les informations sur le fichier téléchargé
+        if (req.file) { // Si true, le fichier a été téléchargé avec succès
+            const imageUrl = `https://backendkarminecorp.tomherault.fr/images/${req.file.filename}`;
+            // Mise à jour du champ 'cover' dans MySQL avec la nouvelle URL de l'image
+            const vetementId = req.body.vetementId; // Assurez-vous d'envoyer l'ID du vêtement avec la requête POST (côté Front)
+            const updateQuery = 'UPDATE vetement SET cover = ? WHERE id = ?';
+            connection.query(updateQuery, [imageUrl, vetementId], (error, results) => {
+                if (error) {
+                    console.error('Erreur mise à jour du champ cover dans MySQL :', error);
+                    res.status(500).json({ error: 'Erreur serveur lors mise à jour image.' });
+                } else {
+                    // Après la MAJ image, on peut renvoyer l'URL de l'image comme réponse si besoin
+                    res.status(200).json({ imageUrl: imageUrl });
+                }
+            });
+        } else {
+            res.status(400).json({ error: 'Aucun fichier téléchargé.' });
+        }
+    } catch (error) {
+        console.error('Erreur mise à jour du champ cover dans MySQL :', error);
+        res.status(500).json({ error: 'Erreur serveur lors mise à jour image.' });
+    }
+});
+
+
+// Route POST pour créer un nouvel utilisateur
+app.post('/signup', (req, res, next) => {
+    const { email, password } = req.body;
+    // Vérifiez si l'email et le mot de passe ont été envoyés
+    if (email && password) {
+        // Hasher le mot de passe avant de l'enregistrer dans la base de données
+        bcrypt.hash(password, 10, (hashError, hashedPassword) => {
+            if (hashError) {
+                console.error('Erreur lors du hachage du mot de passe :', hashError);
+                res.status(500).json({ error: 'Erreur serveur lors création utilisateur.' });
+            } else {
+                connection.query(
+                    'INSERT INTO utilisateur (email, password) VALUES (?, ?)', [email, hashedPassword], (error, results) => {
+                        if (error) {
+                            console.error('Erreur insertion utilisateur dans la base de données :', error);
+                            res.status(500).json({ error: 'Erreur serveur lors création utilisateur.' });
+                        } else {
+                            const userId = results.insertId; // Récupére ID utilisateur nouvellement créé
+                            res.status(201).json({ message: 'Utilisateur créé avec succès.', userId: userId });
+                        }
+                    });
+            }
+        });
+    } else {
+        res.status(400).json({ error: 'Email et mot de passe requis pour inscription.' });
+    }
+});
+
+// Route pour gérer l'authentification de l'utilisateur
+app.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+    // Récupérer le mot de passe haché de l'utilisateur depuis la base de données
+    connection.query('SELECT * FROM utilisateur WHERE email = ?', [email], (error, results) => {
+        if (error) {
+            console.error('Erreur lors recherche utilisateur dans base de données :', error);
+            res.status(500).json({ error: 'Erreur serveur lors authentification.' });
+        } else {
+            if (results.length > 0) {
+                const hashedPasswordFromDB = results[0].password;
+                // Comparer le mot de passe fourni avec le mot de passe haché stocké dans MySQL
+                bcrypt.compare(password, hashedPasswordFromDB, (compareError, match) => {
+                    if (compareError) {
+                        console.error('Erreur lors comparaison des mots de passe :', compareError);
+                        res.status(500).json({ error: 'Erreur serveur lors authentification.' });
+                    } else {
+                        if (match) {
+                            const userId = results[0].id; // 'id' est la colonne contenant l'ID utilisateur
+                            const token = jwt.sign({ userId }, 'votre_clé_secrète', { expiresIn: '24h' }); // Créez un jeton JWT
+                            res.status(200).json({ message: 'Authentification réussie.', userId: userId , token: token, userEmail: email});
+                        } else {
+                            res.status(401).json({ error: 'Identifiants incorrects.' });
+                        }
+                    }
+                });
+            } else {
+                res.status(401).json({ error: 'Identifiants incorrects.' });
+            }
+        }
+    });
+});
+
+module.exports = app;
