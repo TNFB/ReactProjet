755fd8545df9338834762dc2c8dfb1bec154507d
Chimorin <tom.herault4986@gmail.com>
2024-06-05 10:31:28 +0200

    refactor cart + delete item

--- lmf/src/components/Cart.js
+++ lmf/src/components/Cart.js
@@ -3,10 +3,6 @@ import { useState, useEffect } from 'react'
 
 function Cart({ cart, updateCart }) {
 
-    //const tshirtPrice = 8
-    //const pullPrice = 10
-    //const pantalonPrice = 15
-    //const [cart, updateCart] = useState(0)
     const [isOpen, setIsOpen] = useState(false)
     const total = cart.reduce(
         (acc, vetementType) => acc + vetementType.amount * vetementType.price, 0)
@@ -14,26 +10,41 @@ function Cart({ cart, updateCart }) {
         document.title = `LMF: ${total}€ d'achats`
     }, [total])
 
+    function deleteArticle(name) {
+        const currentVetementAdded = cart.find((vetement) => vetement.name === name)
+        if (currentVetementAdded) {
+            if (currentVetementAdded.amount > 1) {
+                const cartFiltered = cart.filter(
+                    (vetement) => vetement.name !== name
+                )
+                updateCart([
+                    ...cartFiltered,
+                    { name, price: currentVetementAdded.price, amount: currentVetementAdded.amount - 1 }
+                ])
+            } else {
+                const cartFiltered = cart.filter(
+                    (vetement) => vetement.name !== name
+                )
+                updateCart([...cartFiltered])
+            }
+        } else {
+            updateCart([...cart, { name, price: currentVetementAdded.price, amount: 1 }])
+        }
+    }
+
     return isOpen ? (
         <div className='lmf-cart'>
             <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
-            {/*<div>
-                        t-shirt : {tshirtPrice}€
-                        <button onClick={() => updateCart(cart + 1)}>
-                        Ajouter
-                        </button>
-                    </div>*/}
             <ul>
                 {cart.map(({ name, price, amount }, index) => (
-                    <div>
+                    <div key={index}>
                         {name} {price}€ x {amount}
+                        <button onClick={() => deleteArticle(name)}>Supprimer</button>
                     </div>
                 ))}
             </ul>
-            {/*<h3>Total : {tshirtPrice * cart }€</h3>*/}
             <h3>Total : {total}€</h3>
-            {/*<button onClick={() => updateCart(0)}>Vider le panier</button>*/}
             <button onClick={() => updateCart([])}>Vider le panier</button>
             <button onClick={() => {
                 localStorage.setItem('cart', JSON.stringify(cart));
@@ -41,7 +52,7 @@ function Cart({ cart, updateCart }) {
             }}>Commander</button>
         </div>
     )
-        : (<button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>)
+        : (<button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>);
 
 }
 export default Cart
